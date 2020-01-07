5b137f08a2e20142c5db23c75474b4984a76898a
Pedro Henrique <pedro00dk@gmail.com>
2020-01-07 17:12:44 -0300

    Add nbody version 1
    Directly unsafe mapping of the C program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod nbodyv1;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        nbodyv1::offset_momentun(nbodyv1::solarBodies.as_mut_ptr());
+        nbodyv1::output_energy(nbodyv1::solarBodies.as_mut_ptr());
+        let c = std::env::args().nth(1).unwrap()  // Note 2
+            .parse().unwrap();
+        for _ in 0..c {
+            nbodyv1::advance(nbodyv1::solarBodies.as_mut_ptr())
+        }
+        nbodyv1::output_energy(nbodyv1::solarBodies.as_mut_ptr());
+    }
 }
